5dbfe1d57e53b79c51a032de4cf4a04fa96b910c
wajahat-creater <47749@students.riphah.edu.pk>
2025-03-05 11:43:04 +0500

    Add files via upload

---
+++ quiz/quiz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+    
+        int[] nums = { 2, 3, 1, 1, 4 };
+        Console.WriteLine(CanJump(nums));
+        Console.ReadLine(); 
+    
+        }
+
+    
+        static bool CanJump(int[] nums)
+    
+        {
+        int lastPos = nums.Length - 1;
+        for (int i = nums.Length - 2; i >= 0; i--)
+            if (i + nums[i] >= lastPos)
+                lastPos = i;
+        return lastPos == 0;
+  
+
+        }
+    }
+}
