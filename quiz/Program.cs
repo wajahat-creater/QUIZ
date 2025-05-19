04d38fb5943ddc4e056de88bf2751c704968e7b6
wajahat-creater <47749@students.riphah.edu.pk>
2025-05-19 22:10:16 +0500

    Add files via upload

---
+++ quiz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 2, 3, 1, 1, 4 };
+        bool canReach = CanJump(nums);
+        Console.WriteLine("Can reach last index: " + canReach);
+    }
+
+    static bool CanJump(int[] nums)
+    {
+        int maxReach = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i > maxReach)
+                return false;
+            maxReach = Math.Max(maxReach, i + nums[i]);
+        }
+        return true;
+    }
+}
+
---
+++ quiz/question2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class question2
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = {
+            {9,1,8},
+            {2,3,7},
+            {6,5,4}
+        };
+
+        Console.WriteLine("Original Matrix:");
+        PrintMatrix(matrix);
+
+        SortMatrix(matrix);
+        Console.WriteLine("\nSorted Matrix:");
+        PrintMatrix(matrix);
+
+        SwapDiagonals(matrix);
+        Console.WriteLine("\nAfter Swapping Diagonals:");
+        PrintMatrix(matrix);
+
+        Console.ReadKey();
+    }
+
+    static void SortMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        int[] temp = new int[rows * cols];
+        int index = 0;
+
+        // Flatten
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                temp[index++] = matrix[i, j];
+
+        // Sort
+        Array.Sort(temp);
+
+        // Fill back
+        index = 0;
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                matrix[i, j] = temp[index++];
+    }
+
+    static void SwapDiagonals(int[,] matrix)
+    {
+        int size = matrix.GetLength(0);
+        for (int i = 0; i < size; i++)
+        {
+            int temp = matrix[i, i];
+            matrix[i, i] = matrix[i, size - 1 - i];
+            matrix[i, size - 1 - i] = temp;
+        }
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+                Console.Write(matrix[i, j] + " ");
+            Console.WriteLine();
+        }
+    }
+}
+
